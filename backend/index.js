7a387d1e3cf004f6a221b94defa16368da3598ee
Md Amanat Ullah <mdamanatullah2003@gmail.com>
2026-03-04 17:08:14 +0530

    update backend index file

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://asrar-perfume-2003.onrender.com/', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
