738c8e232454761c945e21c2aea8c67a456fa4d4
Md Amanat Ullah <mdamanatullah2003@gmail.com>
2026-03-04 17:18:39 +0530

    update project

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,3 @@
-export const API_BASE = (import.meta.env.VITE_API_BASE) ? import.meta.env.VITE_API_BASE : 'http://localhost:5000/api';
+export const API_BASE = import.meta.env.VITE_API_BASE
+  ? import.meta.env.VITE_API_BASE
+  : "https://asrar-perfume-2003.onrender.com";
